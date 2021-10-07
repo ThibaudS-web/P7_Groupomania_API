2aa5723bf4dbf4c5c1a7a392991cf0a6398662ee
ThibaudS <thibaud.devpro@gmail.com>
2021-10-07 10:36:41 +0200

    Fixed static files

--- back-end/middlewares/auth.js
+++ back-end/middlewares/auth.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 //Middleware to require user authentication
-module.exports  =  (req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
         const decodedToken = jwt.verify(token, `${process.env.TOKENPASS}`) //In params, the token and the secret key
         const userId = decodedToken.userId
-        if(req.body.userId && req.body.userId !== userId){
+        if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID not valid !'
         } else {
             req.body.userId = userId
             next()
-            
+
         }
-    } catch ( error ) {
-        res.status(401).json({ error: error | 'Request unauthenticated !'})
+    } catch (error) {
+        res.status(401).json({ error: error | 'Request unauthenticated !' })
     }
 }
 
